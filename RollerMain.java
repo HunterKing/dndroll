5c72168d413fcb1bfa5a4f3dd1247792134657d5
hunter king <hking0036@gmail.com>
2018-04-10 17:59:36 -0400

    initial commit

---
+++ RollerMain.java
@@ -0,0 +1,34 @@
+package dndroll;
+import java.util.Scanner;
+import java.util.Random;
+
+public class RollerMain 
+{
+	public static void main(String[] args)
+	{
+		Random rollRandom = new Random();
+		final int[] DICE_TYPE = {4, 6, 8, 10, 12, 20};
+		int dieChoice = 0;
+		int rollCount = 0;
+		Scanner kb = new Scanner(System.in);
+		while(dieChoice < 6)
+		{
+			System.out.println("What die do you want to roll?");
+			System.out.println("1. d4\n2. d6\n3. d8\n4. d10\n5. d12\n6. d20\n7. Quit");
+			dieChoice = kb.nextInt()-1; //Shift value left to align with array indices.
+			if(dieChoice < 6)
+			{
+				System.out.println("How many would you like to roll?");
+				rollCount = kb.nextInt();
+				int[] result = new int[rollCount];
+				for(int i = 0; i < rollCount; i++)
+				{
+					result[i] = rollRandom.nextInt((DICE_TYPE[dieChoice]));
+					System.out.print(result[i] + " ");
+				}
+				System.out.print("\n");
+			}
+		}
+		kb.close();
+	}
+}
---
+++ readme.md
@@ -0,0 +1,5 @@
+# DnD Roll
+Basic DND dice roller using Java's inbuilt random class. Rolls standard d4, d6, d8, d10, d12, d20.
+
+## To run:
+`java -jar dndRoll.jar`
